e532b3d1730a21b05c8d2e23a332ab160ef248c0
Swan <swan.sallet@epitech.eu>
2026-05-16 13:18:36 +0200

    [ADD] Code

--- cfd_euler.cpp
+++ cfd_euler.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <algorithm>
 #include <sstream>
+#include <chrono>
 
 
 using namespace std;
@@ -18,6 +19,7 @@ const double CFL = 0.5;         // CFL number
 // ------------------------------------------------------------
 // Compute pressure from the conservative variables
 // ------------------------------------------------------------
+#pragma omp declare target
 double pressure(double rho, double rhou, double rhov, double E) {
     double u = rhou / rho;
     double v = rhov / rho;
@@ -28,7 +30,7 @@ double pressure(double rho, double rhou, double rhov, double E) {
 // ------------------------------------------------------------
 // Compute flux in the x-direction
 // ------------------------------------------------------------
-void fluxX(double rho, double rhou, double rhov, double E, 
+void fluxX(double rho, double rhou, double rhov, double E,
            double& frho, double& frhou, double& frhov, double& fE) {
     double u = rhou / rho;
     double p = pressure(rho, rhou, rhov, E);
@@ -50,6 +52,7 @@ void fluxY(double rho, double rhou, double rhov, double E,
     frhov = rhov * v + p;
     fE = (E + p) * v;
 }
+#pragma omp end declare target
 
 // ------------------------------------------------------------
 // Main simulation routine
@@ -65,7 +68,7 @@ int main(){
 
     // Create flat arrays (with ghost cells)
     const int total_size = (Nx + 2) * (Ny + 2);
-    
+
     double* rho = (double*)malloc(total_size * sizeof(double));
     double* rhou = (double*)malloc(total_size * sizeof(double));
     double* rhov = (double*)malloc(total_size * sizeof(double));
@@ -134,111 +137,135 @@ int main(){
     // ----- Time stepping parameters -----
     const int nSteps = 2000;
 
-    // ----- Main time-stepping loop -----
-    for (int n = 0; n < nSteps; n++){
-        // --- Apply boundary conditions on ghost cells ---
-        // Left boundary (inflow): fixed free-stream state
-        for (int j = 0; j < Ny+2; j++){
-            rho[0*(Ny+2)+j] = rho0;
-            rhou[0*(Ny+2)+j] = rho0*u0;
-            rhov[0*(Ny+2)+j] = rho0*v0;
-            E[0*(Ny+2)+j] = E0;
-        }
-        // Right boundary (outflow): copy from the interior
-        for (int j = 0; j < Ny+2; j++){
-            rho[(Nx+1)*(Ny+2)+j] = rho[Nx*(Ny+2)+j];
-            rhou[(Nx+1)*(Ny+2)+j] = rhou[Nx*(Ny+2)+j];
-            rhov[(Nx+1)*(Ny+2)+j] = rhov[Nx*(Ny+2)+j];
-            E[(Nx+1)*(Ny+2)+j] = E[Nx*(Ny+2)+j];
-        }
-        // Bottom boundary: reflective
-        for (int i = 0; i < Nx+2; i++){
-            rho[i*(Ny+2)+0] = rho[i*(Ny+2)+1];
-            rhou[i*(Ny+2)+0] = rhou[i*(Ny+2)+1];
-            rhov[i*(Ny+2)+0] = -rhov[i*(Ny+2)+1];
-            E[i*(Ny+2)+0] = E[i*(Ny+2)+1];
-        }
-        // Top boundary: reflective
-        for (int i = 0; i < Nx+2; i++){
-            rho[i*(Ny+2)+(Ny+1)] = rho[i*(Ny+2)+Ny];
-            rhou[i*(Ny+2)+(Ny+1)] = rhou[i*(Ny+2)+Ny];
-            rhov[i*(Ny+2)+(Ny+1)] = -rhov[i*(Ny+2)+Ny];
-            E[i*(Ny+2)+(Ny+1)] = E[i*(Ny+2)+Ny];
-        }
+    cout << "Starting simulation for " << nSteps << " steps..." << endl;
+    auto t1 = std::chrono::high_resolution_clock::now();
 
-        // --- Update interior cells using a Lax-Friedrichs scheme ---
-        for (int i = 1; i <= Nx; i++){
-            for (int j = 1; j <= Ny; j++){
-                // If the cell is inside the solid obstacle, do not update it
-                if (solid[i*(Ny+2)+j]) {
-                    rho_new[i*(Ny+2)+j] = rho[i*(Ny+2)+j];
-                    rhou_new[i*(Ny+2)+j] = rhou[i*(Ny+2)+j];
-                    rhov_new[i*(Ny+2)+j] = rhov[i*(Ny+2)+j];
-                    E_new[i*(Ny+2)+j] = E[i*(Ny+2)+j];
-                    continue;
+
+    #pragma omp target data map(to: solid[0:total_size]) \
+                            map(tofrom: rho[0:total_size], rhou[0:total_size], rhov[0:total_size], E[0:total_size]) \
+                            map(alloc: rho_new[0:total_size], rhou_new[0:total_size], rhov_new[0:total_size], E_new[0:total_size])
+    {
+        // ----- Main time-stepping loop -----
+        for (int n = 0; n < nSteps; n++){
+            // --- Apply boundary conditions on ghost cells ---
+            // Left boundary (inflow): fixed free-stream state
+            #pragma omp target teams distribute parallel for
+            for (int j = 0; j < Ny+2; j++){
+                rho[0*(Ny+2)+j] = rho0;
+                rhou[0*(Ny+2)+j] = rho0*u0;
+                rhov[0*(Ny+2)+j] = rho0*v0;
+                E[0*(Ny+2)+j] = E0;
+            }
+            // Right boundary (outflow): copy from the interior
+            #pragma omp target teams distribute parallel for
+            for (int j = 0; j < Ny+2; j++){
+                rho[(Nx+1)*(Ny+2)+j] = rho[Nx*(Ny+2)+j];
+                rhou[(Nx+1)*(Ny+2)+j] = rhou[Nx*(Ny+2)+j];
+                rhov[(Nx+1)*(Ny+2)+j] = rhov[Nx*(Ny+2)+j];
+                E[(Nx+1)*(Ny+2)+j] = E[Nx*(Ny+2)+j];
+            }
+            // Bottom boundary: reflective
+            #pragma omp target teams distribute parallel for
+            for (int i = 0; i < Nx+2; i++){
+                rho[i*(Ny+2)+0] = rho[i*(Ny+2)+1];
+                rhou[i*(Ny+2)+0] = rhou[i*(Ny+2)+1];
+                rhov[i*(Ny+2)+0] = -rhov[i*(Ny+2)+1];
+                E[i*(Ny+2)+0] = E[i*(Ny+2)+1];
+            }
+            // Top boundary: reflective
+            #pragma omp target teams distribute parallel for
+            for (int i = 0; i < Nx+2; i++){
+                rho[i*(Ny+2)+(Ny+1)] = rho[i*(Ny+2)+Ny];
+                rhou[i*(Ny+2)+(Ny+1)] = rhou[i*(Ny+2)+Ny];
+                rhov[i*(Ny+2)+(Ny+1)] = -rhov[i*(Ny+2)+Ny];
+                E[i*(Ny+2)+(Ny+1)] = E[i*(Ny+2)+Ny];
+            }
+
+            // --- Update interior cells using a Lax-Friedrichs scheme ---
+            #pragma omp target teams distribute parallel for collapse(2)
+            for (int i = 1; i <= Nx; i++){
+                for (int j = 1; j <= Ny; j++){
+                    // If the cell is inside the solid obstacle, do not update it
+                    if (solid[i*(Ny+2)+j]) {
+                        rho_new[i*(Ny+2)+j] = rho[i*(Ny+2)+j];
+                        rhou_new[i*(Ny+2)+j] = rhou[i*(Ny+2)+j];
+                        rhov_new[i*(Ny+2)+j] = rhov[i*(Ny+2)+j];
+                        E_new[i*(Ny+2)+j] = E[i*(Ny+2)+j];
+                        continue;
+                    }
+
+                    // Compute a Lax averaging of the four neighboring cells
+                    rho_new[i*(Ny+2)+j] = 0.25 * (rho[(i+1)*(Ny+2)+j] + rho[(i-1)*(Ny+2)+j] + 
+                                                rho[i*(Ny+2)+(j+1)] + rho[i*(Ny+2)+(j-1)]);
+                    rhou_new[i*(Ny+2)+j] = 0.25 * (rhou[(i+1)*(Ny+2)+j] + rhou[(i-1)*(Ny+2)+j] + 
+                                                rhou[i*(Ny+2)+(j+1)] + rhou[i*(Ny+2)+(j-1)]);
+                    rhov_new[i*(Ny+2)+j] = 0.25 * (rhov[(i+1)*(Ny+2)+j] + rhov[(i-1)*(Ny+2)+j] + 
+                                                rhov[i*(Ny+2)+(j+1)] + rhov[i*(Ny+2)+(j-1)]);
+                    E_new[i*(Ny+2)+j] = 0.25 * (E[(i+1)*(Ny+2)+j] + E[(i-1)*(Ny+2)+j] + 
+                                            E[i*(Ny+2)+(j+1)] + E[i*(Ny+2)+(j-1)]);
+
+                    // Compute fluxes
+                    double fx_rho1, fx_rhou1, fx_rhov1, fx_E1;
+                    double fx_rho2, fx_rhou2, fx_rhov2, fx_E2;
+                    double fy_rho1, fy_rhou1, fy_rhov1, fy_E1;
+                    double fy_rho2, fy_rhou2, fy_rhov2, fy_E2;
+
+                    fluxX(rho[(i+1)*(Ny+2)+j], rhou[(i+1)*(Ny+2)+j], rhov[(i+1)*(Ny+2)+j], E[(i+1)*(Ny+2)+j],
+                        fx_rho1, fx_rhou1, fx_rhov1, fx_E1);
+                    fluxX(rho[(i-1)*(Ny+2)+j], rhou[(i-1)*(Ny+2)+j], rhov[(i-1)*(Ny+2)+j], E[(i-1)*(Ny+2)+j],
+                        fx_rho2, fx_rhou2, fx_rhov2, fx_E2);
+                    fluxY(rho[i*(Ny+2)+(j+1)], rhou[i*(Ny+2)+(j+1)], rhov[i*(Ny+2)+(j+1)], E[i*(Ny+2)+(j+1)],
+                        fy_rho1, fy_rhou1, fy_rhov1, fy_E1);
+                    fluxY(rho[i*(Ny+2)+(j-1)], rhou[i*(Ny+2)+(j-1)], rhov[i*(Ny+2)+(j-1)], E[i*(Ny+2)+(j-1)],
+                        fy_rho2, fy_rhou2, fy_rhov2, fy_E2);
+
+                    // Apply flux differences
+                    double dtdx = dt / (2 * dx);
+                    double dtdy = dt / (2 * dy);
+                    
+                    rho_new[i*(Ny+2)+j] -= dtdx * (fx_rho1 - fx_rho2) + dtdy * (fy_rho1 - fy_rho2);
+                    rhou_new[i*(Ny+2)+j] -= dtdx * (fx_rhou1 - fx_rhou2) + dtdy * (fy_rhou1 - fy_rhou2);
+                    rhov_new[i*(Ny+2)+j] -= dtdx * (fx_rhov1 - fx_rhov2) + dtdy * (fy_rhov1 - fy_rhov2);
+                    E_new[i*(Ny+2)+j] -= dtdx * (fx_E1 - fx_E2) + dtdy * (fy_E1 - fy_E2);
                 }
-
-                // Compute a Lax averaging of the four neighboring cells
-                rho_new[i*(Ny+2)+j] = 0.25 * (rho[(i+1)*(Ny+2)+j] + rho[(i-1)*(Ny+2)+j] + 
-                                             rho[i*(Ny+2)+(j+1)] + rho[i*(Ny+2)+(j-1)]);
-                rhou_new[i*(Ny+2)+j] = 0.25 * (rhou[(i+1)*(Ny+2)+j] + rhou[(i-1)*(Ny+2)+j] + 
-                                              rhou[i*(Ny+2)+(j+1)] + rhou[i*(Ny+2)+(j-1)]);
-                rhov_new[i*(Ny+2)+j] = 0.25 * (rhov[(i+1)*(Ny+2)+j] + rhov[(i-1)*(Ny+2)+j] + 
-                                              rhov[i*(Ny+2)+(j+1)] + rhov[i*(Ny+2)+(j-1)]);
-                E_new[i*(Ny+2)+j] = 0.25 * (E[(i+1)*(Ny+2)+j] + E[(i-1)*(Ny+2)+j] + 
-                                           E[i*(Ny+2)+(j+1)] + E[i*(Ny+2)+(j-1)]);
-
-                // Compute fluxes
-                double fx_rho1, fx_rhou1, fx_rhov1, fx_E1;
-                double fx_rho2, fx_rhou2, fx_rhov2, fx_E2;
-                double fy_rho1, fy_rhou1, fy_rhov1, fy_E1;
-                double fy_rho2, fy_rhou2, fy_rhov2, fy_E2;
-
-                fluxX(rho[(i+1)*(Ny+2)+j], rhou[(i+1)*(Ny+2)+j], rhov[(i+1)*(Ny+2)+j], E[(i+1)*(Ny+2)+j],
-                      fx_rho1, fx_rhou1, fx_rhov1, fx_E1);
-                fluxX(rho[(i-1)*(Ny+2)+j], rhou[(i-1)*(Ny+2)+j], rhov[(i-1)*(Ny+2)+j], E[(i-1)*(Ny+2)+j],
-                      fx_rho2, fx_rhou2, fx_rhov2, fx_E2);
-                fluxY(rho[i*(Ny+2)+(j+1)], rhou[i*(Ny+2)+(j+1)], rhov[i*(Ny+2)+(j+1)], E[i*(Ny+2)+(j+1)],
-                      fy_rho1, fy_rhou1, fy_rhov1, fy_E1);
-                fluxY(rho[i*(Ny+2)+(j-1)], rhou[i*(Ny+2)+(j-1)], rhov[i*(Ny+2)+(j-1)], E[i*(Ny+2)+(j-1)],
-                      fy_rho2, fy_rhou2, fy_rhov2, fy_E2);
-
-                // Apply flux differences
-                double dtdx = dt / (2 * dx);
-                double dtdy = dt / (2 * dy);
-                
-                rho_new[i*(Ny+2)+j] -= dtdx * (fx_rho1 - fx_rho2) + dtdy * (fy_rho1 - fy_rho2);
-                rhou_new[i*(Ny+2)+j] -= dtdx * (fx_rhou1 - fx_rhou2) + dtdy * (fy_rhou1 - fy_rhou2);
-                rhov_new[i*(Ny+2)+j] -= dtdx * (fx_rhov1 - fx_rhov2) + dtdy * (fy_rhov1 - fy_rhov2);
-                E_new[i*(Ny+2)+j] -= dtdx * (fx_E1 - fx_E2) + dtdy * (fy_E1 - fy_E2);
             }
-        }
 
-        // Copy updated values back
-        for (int i = 1; i <= Nx; i++){
-            for (int j = 1; j <= Ny; j++){
-                rho[i*(Ny+2)+j] = rho_new[i*(Ny+2)+j];
-                rhou[i*(Ny+2)+j] = rhou_new[i*(Ny+2)+j];
-                rhov[i*(Ny+2)+j] = rhov_new[i*(Ny+2)+j];
-                E[i*(Ny+2)+j] = E_new[i*(Ny+2)+j];
+            // Copy updated values back
+            #pragma omp target teams distribute parallel for collapse(2)
+            for (int i = 1; i <= Nx; i++){
+                for (int j = 1; j <= Ny; j++){
+                    rho[i*(Ny+2)+j] = rho_new[i*(Ny+2)+j];
+                    rhou[i*(Ny+2)+j] = rhou_new[i*(Ny+2)+j];
+                    rhov[i*(Ny+2)+j] = rhov_new[i*(Ny+2)+j];
+                    E[i*(Ny+2)+j] = E_new[i*(Ny+2)+j];
+                }
             }
-        }
 
-        // Calculate total kinetic energy
-        double total_kinetic = 0.0;
-        for (int i = 1; i <= Nx; i++) {
-            for (int j = 1; j <= Ny; j++) {
-                double u = rhou[i*(Ny+2)+j] / rho[i*(Ny+2)+j];
-                double v = rhov[i*(Ny+2)+j] / rho[i*(Ny+2)+j];
-                total_kinetic += 0.5 * rho[i*(Ny+2)+j] * (u * u + v * v);
+            // Calculate total kinetic energy
+            double total_kinetic = 0.0;
+            #pragma omp target teams distribute parallel for collapse(2) reduction(+:total_kinetic)
+            for (int i = 1; i <= Nx; i++) {
+                for (int j = 1; j <= Ny; j++) {
+                    double u = rhou[i*(Ny+2)+j] / rho[i*(Ny+2)+j];
+                    double v = rhov[i*(Ny+2)+j] / rho[i*(Ny+2)+j];
+                    total_kinetic += 0.5 * rho[i*(Ny+2)+j] * (u * u + v * v);
+                }
             }
-        }
 
-        // Optional: output progress and write VTK file every 50 time steps
-        if (n % 50 == 0) {
-            cout << "Step " << n << " completed, total kinetic energy: " << total_kinetic << endl;
+            // Optional: output progress and write VTK file every 50 time steps
+            if (n % 50 == 0) {
+                cout << "Step " << n << " completed, total kinetic energy: " << total_kinetic << endl;
+            }
         }
     }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> ms_double = t2 - t1;
+    cout << "Simulation completed in " << ms_double.count() << " ms" << endl;
+
+    free(rho); free(rhou); free(rhov); free(E);
+    free(rho_new); free(rhou_new); free(rhov_new); free(E_new);
+    free(solid);
 
     return 0;
 }
